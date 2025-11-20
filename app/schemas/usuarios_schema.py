735f3c92f14938758a5a1088fc6cb327f5341713
Rochin04 <emirochi22@gmail.com>
2025-11-19 17:19:19 -0700

    feat: Esquema Usuario

---
+++ app/schemas/usuarios_schema.py
@@ -0,0 +1,31 @@
+import uuid
+from pydantic import BaseModel, Field as PydanticField
+from typing import TypeVar, Generic, List
+from app.models.usuarios_model import Usuario
+
+class UsuarioCreate(BaseModel):
+    email: str = PydanticField(..., max_length=255, unique=True)
+    hashed_password: str = PydanticField(..., max_length=255)
+    user_type: str = PydanticField(..., max_length=50)
+    class Config:
+        orm_mode = True
+
+class UsuarioRead(BaseModel):
+    id: uuid.UUID
+    email: str
+    user_type: str
+    class Config:
+        orm_mode = True
+
+T = TypeVar("T")
+class Respons(BaseModel, Generic[T]):
+    data: T
+
+class UsuarioResponse(Respons[Usuario]):
+    pass
+
+class UsuarioReadResponse(Respons[UsuarioRead]):
+    pass
+
+class UsuarioListReadResponse(Respons[List[UsuarioRead]]):
+    pass
