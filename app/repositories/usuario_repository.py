b3e4f85004cc6ad8db8d2e9659baafe24727b57b
Rochin04 <emirochi22@gmail.com>
2025-11-19 17:53:17 -0700

    feat: Repositorio Usuario

---
+++ app/repositories/usuario_repository.py
@@ -0,0 +1,49 @@
+from sqlmodel import Session, select
+from app.models.usuarios_model import Usuario
+from app.schemas.usuarios_schema import UsuarioCreate
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
+import uuid
+
+class UsuarioRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_all_usuarios(self) -> list[Usuario]:
+        return self.session.exec(select(Usuario)).all()
+    
+    def get_usuario_by_id(self, id: uuid.UUID) -> Usuario | None:
+        return self.session.get(Usuario, id)
+    
+    def create_usuario(self, usuario: UsuarioCreate) -> Usuario:
+        db_usuario = Usuario.model_validate(usuario)
+        self.session.add(db_usuario)
+        try:
+            self.session.commit()
+            self.session.refresh(db_usuario)
+        except IntegrityError:
+            self.session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"No se pudo crear el usuario. El usuario con id '{usuario.id}' ya existe."
+            )
+        return db_usuario
+        
+    def update_usuario(self, id: uuid.UUID, usuario_data: UsuarioCreate) -> Usuario | None:
+        db_usuario = self.get_usuario_by_id(id)
+        if db_usuario:
+            db_usuario.email = usuario_data.email
+            db_usuario.hashed_password = usuario_data.hashed_password
+            db_usuario.user_type = usuario_data.user_type
+            self.session.add(db_usuario)
+            self.session.commit()
+            self.session.refresh(db_usuario)
+        return db_usuario
+    
+    def delete_usuario(self, id: uuid.UUID) -> bool:
+        db_usuario = self.get_usuario_by_id(id)
+        if db_usuario:
+            self.session.delete(db_usuario)
+            self.session.commit()
+            return True
+        return False
