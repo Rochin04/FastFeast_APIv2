9e71c37724ef0f5a07a41fdadcd035eec872351e
Rochin04 <emirochi22@gmail.com>
2025-11-20 19:30:51 -0700

    feat: Esquema Estudiante

---
+++ app/schemas/estudiante_schema.py
@@ -0,0 +1,37 @@
+import uuid
+from typing import Optional, TypeVar, Generic, List
+from pydantic import BaseModel
+from app.models.estudiante_model import Estudiante
+
+class EstudianteCreate(BaseModel):
+    user_id: uuid.UUID
+    student_id_number: str
+    full_name: str
+    profile_picture_url: Optional[str] = None
+    is_verified: bool = False
+    
+    class Config:
+        orm_mode = True
+
+class EstudianteRead(BaseModel):
+    # user_id: uuid.UUID
+    student_id_number: str
+    full_name: str
+    profile_picture_url: Optional[str]
+    is_verified: bool
+    
+    class Config:
+        orm_mode = True
+
+T = TypeVar("T")
+class Respons(BaseModel, Generic[T]):
+    data: T
+
+class EstudianteResponse(Respons[Estudiante]):
+    pass
+
+class EstudianteReadResponse(Respons[EstudianteRead]):
+    pass
+
+class EstudianteListReadResponse(Respons[List[EstudianteRead]]):
+    pass
